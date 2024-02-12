a6a9ec111559fdc68b3d1ef9a216fad5b025cf72
Daniel <prime_kkang@naver.com>
2024-02-12 20:48:07 +0900

    [Bronze V] Title: 사분면 고르기, Time: 116 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,29 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        if (a > 0 && b > 0) {
+            System.out.println(1);
+        } else if (a < 0 && b > 0) {
+            System.out.println(2);
+        } else if (a < 0 && b < 0) {
+            System.out.println(3);
+        } else System.out.println(4);
+
+
+    }
+
+}
